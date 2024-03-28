0b6d085e713d139d01babc4c79c4bd29a530213f
Itay David <itayd3104@gmail.com>
2024-03-28 21:15:08 +0200

    adds

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
